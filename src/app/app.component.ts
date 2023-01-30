3179e9e125203b636af002f23126dd9ad080f527
AbdouKhadreDiallo <abdoudiallo405@gmail.com>
2023-01-30 19:59:49 +0000

    end test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,137 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-competences';
+  active = 1
+  startColor ="#0089FF"
+  endColor ="#232532"
+  menus: MenuItem[] = [
+    {
+      label: "АИ-92",
+    },
+    {
+      label: "КЕРОСИН",
+    },
+    {
+      label: "ДТ ЗИМНЕЕ",
+    },
+    {
+      label: "ТС-1",
+    },
+    {
+      label: "АИ-95",
+    },
+    {
+      label: "МАЗУТ",
+    },
+    {
+      label: "ГАЗОЙЛЬ",      
+    },
+    {
+      label: "НЕФТЬ",      
+    }
+  ]
+  reservoirs = [
+    {
+      name: "Macca",
+      numb: "980",
+      percent: 98,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №1",
+      color: "#EB5757"
+    },
+    {
+      name: "Macca",
+      numb: "680",
+      percent: 68,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №2",
+      color: "#00A99D"
+    },
+    {
+      name: "Macca",
+      numb: "180",
+      percent: 18,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №3",
+      color: "#EB5757"
+    },
+    {
+      name: "Macca",
+      numb: "750",
+      percent: 75,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №4",
+      color: "#00A99D"
+    },
+    {
+      name: "Macca",
+      numb: "180",
+      percent: 18,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №3",
+      color: "#EB5757"
+    },
+    {
+      name: "Macca",
+      numb: "750",
+      percent: 75,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №4",
+      color: "#00A99D"
+    },
+    {
+      name: "Macca",
+      numb: "750",
+      percent: 75,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №4",
+      color: "#00A99D"
+    },
+    {
+      name: "Macca",
+      numb: "980",
+      percent: 98,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №1",
+      color: "#EB5757"
+    },
+    {
+      name: "Macca",
+      numb: "680",
+      percent: 68,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №2",
+      color: "#00A99D"
+    },
+    {
+      name: "Macca",
+      numb: "180",
+      percent: 18,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №3",
+      color: "#EB5757"
+    },
+    {
+      name: "Macca",
+      numb: "750",
+      percent: 75,
+      sentence: "Уровень",
+      sentence1: "керосина",
+      sentence2: "в резервуаре №4",
+      color: "#00A99D"
+    },
+  ]
+  gradient(startColor: string, endColor: string, startPercent: number, endPercent: number) {
+    return `linear-gradient(to top, ${startColor} ${startPercent}%, ${endColor} ${endPercent}%)`;
+  }
 }
